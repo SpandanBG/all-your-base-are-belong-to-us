668b218192384e499c4b88ef1657767d481c1485
SpandanBG <spandanbg@gmail.com>
2025-03-31 18:32:34 +0530

    add all file to tests.zig

--- src/tests.zig
+++ src/tests.zig
@@ -1,3 +1,12 @@
 comptime {
     _ = @import("utils.zig");
+    _ = @import("constants.zig");
+    _ = @import("main.zig");
+    _ = @import("repl.zig");
+    _ = @import("sql/processor.zig");
+    _ = @import("sql/constants.zig");
+    _ = @import("sql/tokens/all_tokens.zig");
+    _ = @import("sql/tokens/insert.zig");
+    _ = @import("sql/tokens/select.zig");
+    _ = @import("core/executor.zig");
 }
