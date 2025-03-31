c342a1e0c56fa6adb4da299d3f9d4cf9fd99d925
SpandanBG <spandanbg@gmail.com>
2025-03-31 18:28:57 +0530

    update zig build to run all tests

--- build.zig
+++ build.zig
@@ -22,4 +22,11 @@ pub fn build(b: *std.Build) void {
 
     const run_step = b.step("run", "Run the application");
     run_step.dependOn(&run_exe.step);
+
+    // Setup Test
+    const test_exe = b.addTest(.{ .root_source_file = b.path("src/tests.zig") });
+    const tests = b.addRunArtifact(test_exe);
+
+    const run_test = b.step("test", "Run unit test");
+    run_test.dependOn(&tests.step);
 }
